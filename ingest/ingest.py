d22f55e0f9dae73567698953fbc1495037b56eb7
Joman Chu <code@notatypewriter.com>
2012-05-03 23:33:04 -0400

    ingest parses the json object TODO output to csv

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -2,24 +2,45 @@
 
 import sys
 import base64
+import json
 
 # Delimeter between form responses
 ENTRYDELIM = '----'
 
-# Delimeter between fields in the POST string
-FIELDDELIM = '&'
-
-# List of attributes we want to get in the POST string
-ATTRIBUTES = ['course', 'cont', 'labs', 'org', 'course-comment', 'name', 'sid']
-
-# List of attributes we want to see in the output CSV
-CSVHEADER = ATTRIBUTES + ['instructors']
+# List of objects we expect from the JSON.
+# List of attributes and the order in which we want them
+# in the output CSV. Values in this list are the names of
+# fields in the JSON object. Strings are names of scalar
+# fields. Objects in the JSON object are represented as
+# maps with one tuple. The key of that tuple is the name
+# of the field in the JSON object and the value of that
+# tuple is a list of fields in the object. This structure
+# is parsed recursively so it can be nested as many times
+# as neccessary.
+INST_ATTRS = ['name', 'knowledge', 'prep', 'comm', 'comment' ] 
+RESP_ATTRS = ['course', 'content', 'labs', 'organization', 'courseComment', 'name', 'sid', {'instructors' : INST_ATTRS }]
+ATTRIBUTES = [RESP_ATTRS, INST_ATTRS]
 
 # Maintain a list of instructors here so that the CSV can have all instructors
-instructors = []
+instructors = set([])
 
 # List of parsed responses
-entries = []
+responses = []
+
+def processJSONobject(obj):
+  def testfunc(a):
+    if type(a) == dict:
+      key = a.keys()[0]
+      return key in obj
+    else:
+      return a in obj
+  # The JSON object must match one of the predefined objects.
+  for attrs in ATTRIBUTES:
+    bools = map(lambda a: testfunc(a), attrs);
+    if not (False in bools):
+      return obj
+  print >> sys.stderr, "%s did not match any objects" % (data)
+  return None
 
 # Parse a single response
 def processLine(s):
@@ -29,32 +50,25 @@ def processLine(s):
     return
 
   data = base64.b64decode(s)
-  data = data.split('&')
-  data = map(lambda x: x.split('=', 1), data)
-  data = filter(lambda x: len(x) == 2, data)
-
-  # Right now we do a very not robust thing where if the attribute ends in a
-  # digit, we assume that it's data for an instructor.
-  entry = dict()
-  for (attr, val) in data:
-    if attr[0:9] == 'instname-':
-      if val not in instructors:
-        instructors += [val]
-      if 'instructors' not in entry.keys():
-        entry['instructors'] = dict()
-      entry['instructors'][val] = dict()
-    elif attr in ATTRIBUTES:
-      entry[attr] = val
-    else:
-      print "Unknown attribute found: %s" % (attr)
-
-  entries.append(entry)
-
-  print data
+  data = json.loads(data, object_hook=processJSONobject)
+  responses.append(data)
+  for i in data['instructors']:
+    instructors.add(i['name'])
 
 # Dump data to CSV
 def dumpCSV():
-  print entries
+  global instructors
+  instructors = sorted(instructors)
+  print responses
+  print instructors
+
+  # create header row
+  hdr = [e for e in RESP_ATTRS if type(e) == str];
+  for e in [e for e in RESP_ATTRS if type(e) == dict]:
+    key = e.keys()[0]
+    name = instructors[i]
+    hdr += [name+'-'+s for s in e[key]]*len(instructors)
+  print hdr
 
 if __name__ == "__main__":
 
@@ -82,6 +96,5 @@ if __name__ == "__main__":
 
   f.close()
 
-  print instructors
   dumpCSV()
 
